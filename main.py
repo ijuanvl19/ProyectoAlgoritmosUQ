8f78300d234424786fccad676a86a8dd79b72488
Juan <juanp.velezl19@gmail.com>
2025-05-18 20:28:05 -0500

    ajustes para refresh fronted

--- main.py
+++ main.py
@@ -25,86 +25,86 @@ import data_model.categoreis_lists as cl
 # thread_3.join()
 
 
-# Especifica la carpeta que contiene los archivos .bib
-folders = ['IEEE', 'sage', 'sciense']
+# # Especifica la carpeta que contiene los archivos .bib
+# folders = ['IEEE', 'sage', 'sciense']
 
-output_folder = 'assets/temps/'
+# output_folder = 'assets/temps/'
 
-for folder in folders:
-    # Obtener la ruta de la carpeta actual
-    routs = [os.path.normpath(r).replace('\\', '/') for r in cd.obtener_archivos_bib('assets/' + folder)]
-    # Crear la ruta completa para el archivo de salida
-    output_filepath = os.path.join(output_folder, f'{folder}Consolidated.bib')
-    # Combinar los archivos .bib y guardar en la ruta especificada
-    cd.combinar_bibtex_sin_repetidos_por_titulo(routs, output_filepath)
-    print(f"Archivo combinado sin repetidos guardado en: {output_filepath}")
+# for folder in folders:
+#     # Obtener la ruta de la carpeta actual
+#     routs = [os.path.normpath(r).replace('\\', '/') for r in cd.obtener_archivos_bib('assets/' + folder)]
+#     # Crear la ruta completa para el archivo de salida
+#     output_filepath = os.path.join(output_folder, f'{folder}Consolidated.bib')
+#     # Combinar los archivos .bib y guardar en la ruta especificada
+#     cd.combinar_bibtex_sin_repetidos_por_titulo(routs, output_filepath)
+#     print(f"Archivo combinado sin repetidos guardado en: {output_filepath}")
 
-print("Archivos combinados y guardados correctamente.")
+# print("Archivos combinados y guardados correctamente.")
 
-# generar un archivo bibtex consolidado sin repetidos por título general de los archivos .bib temporales
-input_folder = 'assets/temps/'
-output_filepath = os.path.join(input_folder, 'Consolidated.bib')
+# # generar un archivo bibtex consolidado sin repetidos por título general de los archivos .bib temporales
+# input_folder = 'assets/temps/'
+# output_filepath = os.path.join(input_folder, 'Consolidated.bib')
 
-# Combinar los archivos .bib y guardar en la ruta especificada
-cd.combinar_bibtex_sin_repetidos_por_titulo(cd.obtener_archivos_bib(input_folder), output_filepath)
-print(f"Archivo combinado sin repetidos guardado en: {output_filepath}")
+# # Combinar los archivos .bib y guardar en la ruta especificada
+# cd.combinar_bibtex_sin_repetidos_por_titulo(cd.obtener_archivos_bib(input_folder), output_filepath)
+# print(f"Archivo combinado sin repetidos guardado en: {output_filepath}")
 # -------------------------------------------------------------------------------------------------------
 # hasta aqui el codigo de la parte de scrapping y limpieza de los archivos bibtex
 # -------------------------------------------------------------------------------------------------------
 
 
 # Graficar los datos de las bases de datos
-# input_file = 'assets/temps/Consolidated.bib'
+input_file = 'assets/temps/Consolidated.bib'
 
-# data = cd.leer_bibtex_con_mmap_regex(input_file)
+data = cd.leer_bibtex_con_mmap_regex(input_file)
 
 # graficas consideradas core en el documento de requerimiento
 # top 15 autores
 ''' esta funcion grafica los 15 autores con mayor cantidad de documentos mostrando el nombre y la cantidad de documentos en los que han participado'''
-# gr.graficate_authors(data)
+gr.graficate_authors(data)
 
 # Graficar los datos de los años
 ''' esta funcion grafica la cantidad de documentos por año en una linea de tiempo'''
-# gr.graficate_year(data)
+gr.graficate_year(data)
 
 # graficar los diferetnes tipos de documentos por año
 ''' esta funcion grafica la cantidad de documentos por año y por tipo de documento(articulo, libro, etc)'''
-# gr.graficar_por_anio_y_tipo(data)
+gr.graficar_por_anio_y_tipo(data)
 
 # Graficar los datos de los tipos de documentos
 ''' esta funcion grafica la cantidad de documentos por tipo de documento(articulo, libro, etc)'''
-# gr.graficate_entertype(data)
+gr.graficate_entertype(data)
 
 # graficar los 15 journals con mayor cantidad de documentos
 ''' esta funcion grafica los 15 journals con mayor cantidad de documentos mostrando el nombre y la cantidad de documentos en los que han participado'''
-# gr.graficate_journals(data)
+gr.graficate_journals(data)
 
 # graficar las 15 revistas con mayor cantidad de documentos
 ''' esta funcion grafica los 15 revistas con mayor cantidad de documentos mostrando el nombre y la cantidad de documentos en los que han participado'''
-# gr.graficate_publisher(data)
+gr.graficate_publisher(data)
 # --------------------------------------------------------------------------------------------------------
 # hasta aqui las graficas core
 # --------------------------------------------------------------------------------------------------------
 # graficas adicionales que consideramos relevantes
 # Llamar a la función para graficar las bases de datos
 ''' esta funcion grafica la cantidad de documentos por base de datos'''
-# gr.graficate_databases(data)
+gr.graficate_databases(data)
 
 # graficar los diferentes tipos de documentos por base de datos
 ''' esta funcion grafica la cantidad de documentos por base de datos y por tipo de documento(articulo, libro, etc)'''
-# gr.graficate_type_database(data)
+gr.graficate_type_database(data)
 
 # graficar los diferentes tipos de documentos por año
 ''' esta funcion grafica la cantidad de documentos por año y por tipo de documento(articulo, libro, etc) mostrando un mapa de calor'''
-# gr.heatmap_tipo_vs_anio(data)
+gr.heatmap_tipo_vs_anio(data)
 # ----------------------------------------------------------------------------------------------------------
 # hasta aqui las graficas adicionales
 # ----------------------------------------------------------------------------------------------------------
 
 # crear tabla de contenido de las categorias
-# gr.generate_table_words(data)
+gr.generate_table_words(data)
 
 # graficar nubes de palabras y tablas de frecuencia para cada categoria
 ''' esta funcion grafica una nube de palabras y una tabla de frecuencia para cada categoria asi como una tabla de contenido general'''
-# gr.generate_table_words(data)
+gr.generate_table_words(data)
 
